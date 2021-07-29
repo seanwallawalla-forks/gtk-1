8500602eae99f3a2e920ba48ced26657cde2a057
Chun-wei Fan <fanchunwei@src.gnome.org>
2021-07-29 18:35:08 +0800

    GDK-Win32: Make surface ready for updates
    Make the toplevel surface respond to size computations unless it is just being
    created, or maximized, made fullscreen or underwent an AeroSnap operation.
    
    This will ensure that the surface size is properly computed in time, so that
    surfaces can be resized as needed.
    
    This will fix issues 3728 and 3799.

--- gdk/win32/gdksurface-win32.c
+++ gdk/win32/gdksurface-win32.c
@@ -655,6 +655,7 @@ _gdk_win32_display_create_surface (GdkDisplay     *display,
 
   g_object_unref (frame_clock);
   impl->hdc = GetDC (impl->handle);
+  impl->inhibit_configure = TRUE;
 
   return surface;
 }
@@ -1096,7 +1097,8 @@ gdk_win32_surface_resize (GdkSurface *window,
                            SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOZORDER));
   window->resize_count += 1;
 
-  gdk_surface_request_layout (window);
+  if (!GDK_WIN32_SURFACE (window)->force_recompute_size)
+    gdk_surface_request_layout (window);
 }
 
 static void
@@ -2549,6 +2551,12 @@ apply_snap (GdkSurface             *window,
       snap_up (window);
       break;
     }
+
+  if (snap != GDK_WIN32_AEROSNAP_STATE_UNDETERMINED)
+    {
+      GDK_WIN32_SURFACE (window)->inhibit_configure = TRUE;
+      GDK_WIN32_SURFACE (window)->force_recompute_size = FALSE;
+    }
 }
 
 /* Registers a dumb window class. This window
@@ -3444,6 +3452,7 @@ setup_drag_move_resize_context (GdkSurface                   *window,
   GdkWin32Surface *impl = GDK_WIN32_SURFACE (window);
   gboolean maximized = gdk_toplevel_get_state (GDK_TOPLEVEL (window)) & GDK_TOPLEVEL_STATE_MAXIMIZED;
   int root_x, root_y;
+  gboolean restore_configure = FALSE;
 
   gdk_win32_surface_get_root_coords (window, x, y, &root_x, &root_y);
 
@@ -3492,6 +3501,7 @@ setup_drag_move_resize_context (GdkSurface                   *window,
        impl->snap_state == GDK_WIN32_AEROSNAP_STATE_FULLUP))
     {
       discard_snapinfo (window);
+      restore_configure = TRUE;
     }
   else if (maximized ||
            (impl->snap_state == GDK_WIN32_AEROSNAP_STATE_HALFRIGHT ||
@@ -3506,6 +3516,7 @@ setup_drag_move_resize_context (GdkSurface                   *window,
       gboolean left_half;
       GdkDisplay *display;
 
+      restore_configure = TRUE;
       display = gdk_surface_get_display (window);
       monitor = gdk_display_get_monitor_at_surface (display, window);
       gdk_surface_get_geometry (window, &wx, &wy, &wwidth, &wheight);
@@ -3675,6 +3686,9 @@ setup_drag_move_resize_context (GdkSurface                   *window,
           SetCursorPos (root_x - _gdk_offset_x, root_y - _gdk_offset_y);
         }
     }
+
+  if (restore_configure)
+    impl->inhibit_configure = FALSE;
 
   _gdk_win32_get_window_rect (window, &rect);
 
@@ -4130,6 +4144,10 @@ gdk_win32_surface_maximize (GdkSurface *window)
 			   GDK_SURFACE_HWND (window),
 			   _gdk_win32_surface_state_to_string (window->state)));
 
+  impl = GDK_WIN32_SURFACE (window);
+  impl->inhibit_configure = TRUE;
+  impl->force_recompute_size = FALSE;
+
   if (GDK_SURFACE_IS_MAPPED (window))
     GtkShowWindow (window, SW_MAXIMIZE);
   else
@@ -4141,6 +4159,8 @@ gdk_win32_surface_maximize (GdkSurface *window)
 static void
 gdk_win32_surface_unmaximize (GdkSurface *window)
 {
+  GdkWin32Surface *impl;
+
   g_return_if_fail (GDK_IS_SURFACE (window));
 
   if (GDK_SURFACE_DESTROYED (window))
@@ -4158,6 +4178,14 @@ gdk_win32_surface_unmaximize (GdkSurface *window)
     gdk_synthesize_surface_state (window,
 				 GDK_TOPLEVEL_STATE_MAXIMIZED,
 				 0);
+
+  impl = GDK_WIN32_SURFACE (window);
+
+  if (impl->inhibit_configure)
+    {
+      impl->inhibit_configure = FALSE;
+      impl->force_recompute_size = TRUE;
+    }
 }
 
 static void
@@ -4582,6 +4610,9 @@ _gdk_win32_surface_request_layout (GdkSurface *surface)
                                          &surface->x, &surface->y,
                                           NULL, NULL);
         }
+
+      if (!impl->inhibit_configure)
+        impl->force_recompute_size = TRUE;
     }
 }
 
@@ -4596,8 +4627,18 @@ _gdk_win32_surface_compute_size (GdkSurface *surface)
 
   if (!impl->drag_move_resize_context.native_move_resize_pending)
     {
-      surface->width = impl->next_layout.configured_width;
-      surface->height = impl->next_layout.configured_height;
+      if (GDK_IS_TOPLEVEL (surface) && impl->force_recompute_size)
+        {
+          surface->width = width;
+          surface->height = height;
+          gdk_win32_surface_resize (surface, width, height);
+          impl->force_recompute_size = FALSE;
+        }
+      else
+        {
+          surface->width = impl->next_layout.configured_width;
+          surface->height = impl->next_layout.configured_height;
+        }
 
       _gdk_surface_update_size (surface);
     }
